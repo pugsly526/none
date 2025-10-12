3588e9dba415a8296e329537c45e363df879423d
pugsly526 <pugsly526@gmail.com>
2025-10-12 22:28:07 +0300

    Add files via upload

---
+++ lee.js
@@ -0,0 +1,20 @@
+var commands = [];
+
+function lee(info, func) {
+    var data = info;
+    data.function = func;
+    if (!data.dontAddCommandList) data.dontAddCommandList = false;
+    if (!info.desc) info.desc = '';
+    if (!data.fromMe) data.fromMe = false;
+    if (!info.category) data.category = 'misc';
+    if(!info.filename) data.filename = "Not Provided";
+    commands.push(data);
+    return data;
+}
+module.exports = {
+    lee,
+    AddCommand:lee,
+    Function:lee,
+    Module:lee,
+    commands,
+};
